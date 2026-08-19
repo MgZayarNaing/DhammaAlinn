bc2aa79e098c2e63bcb8b06a3773c7cbe618d70a
Zayar Naing <zayarnaing.st@gmail.com>
2026-08-19 19:42:36 +0630

    frame

---
+++ src/endpoints/endpoints.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const endpoints = () => {
+  return (
+    <View>
+      <Text>endpoints</Text>
+    </View>
+  )
+}
+
+export default endpoints
---
+++ src/navigation/RootNaviagtion.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RootNaviagtion = () => {
+  return (
+    <View>
+      <Text>RootNaviagtion</Text>
+    </View>
+  )
+}
+
+export default RootNaviagtion
